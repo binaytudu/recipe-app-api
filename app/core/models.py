be11217bfc0cc915d8362bb01ac4f52437f020bf
bini <bini@example.com>
2020-04-10 15:53:21 +0530

    Added tag list endpoint

--- app/core/models.py
+++ app/core/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, \
-                                            BaseUserManager, PermissionsMixin       
-class UserManager(BaseUserManager):
+                                            BaseUserManager, PermissionsMixin
+from django.conf import settings
 
+
+class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """ Creates and saves a new user """
         if not email:
             raise ValueError('Users must have an email address must')
-        user = self.model(email=self.normalize_email(email), **extra_fields)  # new user model 
+        # new user model
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -16,7 +19,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         user = self.create_user(email, password)
-        user.is_staff= True
+        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
@@ -25,12 +28,23 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """ Custom user model that supports using email instead of username"""
-    email= models.EmailField(max_length= 255, unique= True)
+    email = models.EmailField(max_length= 255, unique= True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff= models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
 
 
-    objects= UserManager()
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
 
-    USERNAME_FIELD= 'email'
+    def __str__(self):
+        return self.name
--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -1,19 +1,27 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@londonappdev.com', password='testpass'):
+        """Create a sample user"""
+
+        return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """  Test creating a new user with an email is successful """
-        email ='test@indiaappdev.com'
-        password= 'Testpass123'
+        email = 'test@indiaappdev.com'
+        password = 'Testpass123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
 
-        self.assertEqual(user.email,email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
@@ -40,3 +48,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
